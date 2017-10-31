6e9f096bc0fb9d1d71ee7753d7d869dd73aa1f64
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2017-10-31 08:23:48 +0000

    ATED-3782: Update 3 links from div to p tags.

--- app/controllers/reliefs/ReliefsSummaryController.scala
+++ app/controllers/reliefs/ReliefsSummaryController.scala
@@ -43,6 +43,9 @@ trait ReliefsSummaryController extends BackLinkController
             backLink <- currentBackLink
             retrievedData <- reliefsService.retrieveDraftReliefs(atedContext.user.atedReferenceNumber, periodKey)
           } yield {
+            println("*********************************")
+            println(retrievedData.toString)
+            println("*********************************")
             val canSubmit = AtedUtils.canSubmit(periodKey, LocalDate.now)
             Ok(views.html.reliefs.reliefsSummary(retrievedData.map(_.periodKey).getOrElse(periodKey),
               retrievedData, canSubmit,
