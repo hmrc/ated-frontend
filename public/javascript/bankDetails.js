b7e90ecb8c63ba5931708a5ca7a065908d81963e
robert.lavender <robert.lavender@digital.hmrc.gov.uk>
2017-11-16 15:38:39 +0000

    ATED-3935: Small tweak to sort code helper.

--- public/javascript/bankDetails.js
+++ public/javascript/bankDetails.js
@@ -6,9 +6,11 @@ function showHideBankDetails() {
 
     $('input[type=radio][name=hasUKBankAccount]').change(function(){
         if(this.value == 'true') {
+            $('#non-uk-accountName').val("");
             $('#iban').val("");
             $('#bicSwiftCode').val("");
         } else {
+            $('#accountName').val("");
             $('#sortCode_firstElement').val("");
             $('#sortCode_secondElement').val("");
             $('#sortCode_thirdElement').val("");
