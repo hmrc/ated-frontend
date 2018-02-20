1e32bb53ea17d00a6471401077eb1c500157544e
shane.wilson <shane.wilson@digital.gmrc.gov.uk>
2018-02-20 13:41:03 +0000

    DDCNLS-2731 made initial changes to change dates from 2012 to 2017 and configured it to be needed to change once WIP

--- test/controllers/propertyDetails/PropertyDetailsOwnedBeforeControllerSpec.scala
+++ test/controllers/propertyDetails/PropertyDetailsOwnedBeforeControllerSpec.scala
@@ -35,6 +35,7 @@ import play.api.test.Helpers._
 import services.{PropertyDetailsCacheSuccessResponse, PropertyDetailsService}
 import uk.gov.hmrc.play.frontend.auth.connectors.{AuthConnector, DelegationConnector}
 import utils.AtedConstants
+import config.ApplicationConfig.valuationYear
 
 import scala.concurrent.Future
 
@@ -102,13 +103,13 @@ class PropertyDetailsOwnedBeforeControllerSpec extends PlaySpec with OneServerPe
             result =>
               status(result) must be(OK)
               val document = Jsoup.parse(contentAsString(result))
-              document.getElementById("property-details-header").text() must be("Did the company own this property on or before 1 April 2012?")
+              document.getElementById("property-details-header").text() must be (s"Did the company own this property on or before 1 April $valuationYear?")
 
-              document.getElementById("isOwnedBefore2012").text() must be("Did the company own this property on or before 1 April 2012? Yes No")
-              document.getElementById("ownedBefore2012Value_field").text() must be("What was the value of the property on the 1 April 2012?")
-              document.getElementById("ownedBefore2012Value").attr("type") must be("text")
-              document.getElementById("isOwnedBefore2012-true").attr("checked") must be("")
-              document.getElementById("isOwnedBefore2012-false").attr("checked") must be("")
+              document.getElementById("isOwnedBefore2012").text() must be (s"Did the company own this property on or before 1 April $valuationYear? Yes No")
+              document.getElementById("ownedBefore2012Value_field").text() must be (s"What was the value of the property on the 1 April $valuationYear?")
+              document.getElementById("ownedBefore2012Value").attr("type") must be ("text")
+              document.getElementById("isOwnedBefore2012-true").attr("checked") must be ("")
+              document.getElementById("isOwnedBefore2012-false").attr("checked") must be ("")
 
           }
         }
@@ -125,7 +126,7 @@ class PropertyDetailsOwnedBeforeControllerSpec extends PlaySpec with OneServerPe
             result =>
               status(result) must be(OK)
               val document = Jsoup.parse(contentAsString(result))
-              document.getElementById("property-details-header").text() must be("Did the company own this property on or before 1 April 2012?")
+              document.getElementById("property-details-header").text() must be (s"Did the company own this property on or before 1 April $valuationYear?")
 
               document.getElementById("backLinkHref").text must be("Back")
               document.getElementById("backLinkHref").attr("href") must include("/ated/liability/create/summary")
