b5b9eb2dbb3e54abe562885fe84fc802cff7ccbc
Gaurav Gaur <gaurav.gaur@digital.hmrc.gov.uk>
2018-11-22 15:18:56 +0000

    DDCNLS-5118 introducing AtedViewSpec to test the views

---
+++ test/utils/viewHelpers/JsoupMatchers.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils.viewHelpers
+
+import org.jsoup.nodes.Document
+import org.scalatest.matchers.{MatchResult, Matcher}
+
+trait JsoupMatchers {
+
+  import scala.collection.JavaConversions._
+
+  class TagWithTextMatcher(expectedContent: String, tag: String) extends Matcher[Document] {
+    def apply(left: Document): MatchResult = {
+      val elements: List[String] =
+        left.getElementsByTag(tag)
+          .toList
+          .map(_.text)
+
+      lazy val elementContents = elements.mkString("\t", "\n\t", "")
+
+      MatchResult(
+        elements.contains(expectedContent),
+        s"[$expectedContent] not found in '$tag' elements:[\n$elementContents]",
+        s"'$tag' element found with text [$expectedContent]"
+      )
+    }
+  }
+
+  def haveHeadingWithText (expectedText: String) = new TagWithTextMatcher(expectedText, "h1")
+}
