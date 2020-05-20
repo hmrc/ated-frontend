b7867916c8fa36b523a92a759b6b3cd7aa80a014
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2020-05-20 10:49:01 +0100

    DL-3236 update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,9 +6,9 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.7.0",
+     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.8.0",
     "uk.gov.hmrc" %% "auth-client" % "3.0.0-play-26",
-    "uk.gov.hmrc" %% "play-ui" % "8.9.0-play-26",
+    "uk.gov.hmrc" %% "play-ui" % "8.10.0-play-26",
     "uk.gov.hmrc" %% "play-partials" % "6.11.0-play-26",
     "uk.gov.hmrc" %% "domain" % "5.9.0-play-26",
     "uk.gov.hmrc" %% "http-caching-client" % "9.0.0-play-26",
