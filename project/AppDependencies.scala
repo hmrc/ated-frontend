3fc6cad0a70ce89651d7aebc6e3ee4776b712da1
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2021-01-20 15:28:19 +0000

    DL-4813 dependency updates applied

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,9 +6,9 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.2.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % "3.3.0",
     "uk.gov.hmrc" %% "auth-client" % "3.2.0-play-27",
-    "uk.gov.hmrc" %% "play-ui" % "8.20.0-play-27",
+    "uk.gov.hmrc" %% "play-ui" % "8.21.0-play-27",
     "uk.gov.hmrc" %% "play-partials" % "7.1.0-play-27",
     "uk.gov.hmrc" %% "domain" % "5.10.0-play-27",
     "uk.gov.hmrc" %% "http-caching-client" % "9.2.0-play-27",
