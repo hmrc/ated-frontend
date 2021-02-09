67ed2e28763d9367599c49dd43e3fc44dd02aecc
marjan <marjan.mubarok@digital.hmrc.gov.uk>
2021-02-09 10:02:49 +0000

    DL-3195 Add unit test for avoidance scheme details page

---
+++ test/views/reliefs/avoidanceSchemes.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.reliefs
+
+import builders.ReliefBuilder.reliefTaxAvoidance
+import config.ApplicationConfig
+import forms.ReliefForms._
+import models.StandardAuthRetrievals
+import org.jsoup.Jsoup
+import org.scalatest.{BeforeAndAfterEach, FeatureSpec, GivenWhenThen}
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.i18n.{Messages, MessagesApi}
+import play.api.test.FakeRequest
+import play.twirl.api.Html
+import testhelpers.MockAuthUtil
+
+class avoidanceSchemes extends FeatureSpec with GuiceOneAppPerSuite
+  with MockitoSugar with BeforeAndAfterEach with GivenWhenThen with MockAuthUtil {
+
+  implicit val request = FakeRequest()
+  implicit val messages: Messages = app.injector.instanceOf[MessagesApi].preferred(request)
+
+  implicit val mockAppConfig: ApplicationConfig = app.injector.instanceOf[ApplicationConfig]
+  implicit lazy val authContext: StandardAuthRetrievals = organisationStandardRetrievals
+  val periodKey = 2015
+
+  val injectedViewInstance = app.injector.instanceOf[views.html.reliefs.avoidanceSchemes]
+
+  feature("The user can view the Enter your avoidance scheme number page") {
+
+    info("as a client i want to be able to enter the details of my avoidance scheme")
+
+    scenario("show the input boxes so the user can enter their avoidance scheme details ") {
+
+      Given("the client has answered 'yes' to an avoidance scheme being used")
+      When("The user views the page")
+
+      val html = injectedViewInstance(periodKey, taxAvoidanceForm, Html(""), Some("backLink"))(Some(reliefTaxAvoidance(periodKey)))
+
+      val document = Jsoup.parse(html.toString())
+
+      Then("The header should match - Enter your avoidance scheme number")
+      assert(document.select("h1").text === "Enter your avoidance scheme number")
+
+      Then("There should be a label describing the relief claimed")
+      assert(document.getElementById("relief-summary-text").text() === "Reliefs claimed")
+
+      Then("There should be a label with the text Avoidance scheme reference number")
+      assert(document.getElementById("relief-summary-scheme-text").text() === "Avoidance scheme reference number")
+
+      Then("There should be a label with text Promoter reference number")
+      assert(document.getElementById("relief-summary-scheme-promoter-text").text() === "Promoter reference number")
+
+      Then("The back link is correct")
+      assert(document.getElementById("backLinkHref").text === "Back")
+    }
+  }
+}
