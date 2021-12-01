1c878013016cb49536dce1956a0dd9647c89f1aa
nadinJ <63252254+nadinJ@users.noreply.github.com>
2021-12-01 17:25:56 +0000

    DL-6831: Created layout

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,13 +8,14 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "5.16.0",
-    "uk.gov.hmrc"       %% "play-ui"                    % "9.7.0-play-28",
-    "uk.gov.hmrc"       %% "play-partials"              % "8.2.0-play-28",
-    "uk.gov.hmrc"       %% "domain"                     % "6.2.0-play-28",
-    "uk.gov.hmrc"       %% "http-caching-client"        % "9.5.0-play-28",
-    "com.typesafe.play" %% "play-json-joda"             % "2.7.4",
-    "uk.gov.hmrc"       %% "govuk-template"             % "5.72.0-play-28"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.16.0",
+    "uk.gov.hmrc" %% "play-ui" % "9.7.0-play-28",
+    "uk.gov.hmrc" %% "play-partials" % "8.2.0-play-28",
+    "uk.gov.hmrc" %% "domain" % "6.2.0-play-28",
+    "uk.gov.hmrc" %% "http-caching-client" % "9.5.0-play-28",
+    "com.typesafe.play" %% "play-json-joda" % "2.7.4",
+    "uk.gov.hmrc" %% "govuk-template" % "5.72.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "1.26.0-play-28"
   )
 
   trait TestDependencies {
@@ -25,15 +26,15 @@ object AppDependencies {
   object Test {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test: Seq[ModuleID] = Seq(
-        "uk.gov.hmrc"            %% "bootstrap-test-play-28"      % "5.16.0"            % scope,
-        "org.scalatestplus.play" %% "scalatestplus-play"          % "5.1.0"             % scope,
-        "org.pegdown"            %  "pegdown"                     % "1.6.0"             % scope,
-        "org.jsoup"              %  "jsoup"                       % "1.14.3"            % scope,
-        "com.typesafe.play"      %% "play-test"                   % PlayVersion.current % scope,
-        "org.scalatestplus"      %% "mockito-3-12"                % "3.2.10.0"          % scope,
-        "org.mockito"            %  "mockito-core"                % "4.0.0"             % scope,
-        "com.github.tomakehurst" %  "wiremock-jre8"               % "2.26.3"            % scope,
-        "org.scalamock"          %% "scalamock-scalatest-support" % "3.6.0"             % scope
+        "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.16.0" % scope,
+        "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % scope,
+        "org.pegdown" % "pegdown" % "1.6.0",
+        "org.jsoup" % "jsoup" % "1.14.3" % scope,
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
+        "org.scalatestplus" %% "mockito-3-12" % "3.2.10.0" % scope,
+        "org.mockito" % "mockito-core" % "4.0.0" % scope,
+        "com.github.tomakehurst" % "wiremock-jre8" % "2.26.3" % "test,it",
+        "org.scalamock" %% "scalamock-scalatest-support" % "3.6.0" % scope
       )
     }.test
   }
