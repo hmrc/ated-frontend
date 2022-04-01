f59074087f6f905c6c18c43376c07c5295c5c998
nadinJ <63252254+nadinJ@users.noreply.github.com>
2022-04-01 10:04:35 +0100

    DL-6836: Changes for frontend migration

--- test/views/html/helpers/peakGuidanceSpec.scala
+++ test/views/html/helpers/peakGuidanceSpec.scala
@@ -17,68 +17,41 @@
 package views.html.helpers
 
 import config.ApplicationConfig
-import models.StandardAuthRetrievals
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.scalatest.{FeatureSpec, GivenWhenThen}
-import org.scalatestplus.play.guice.GuiceOneAppPerTest
-import play.api.i18n.{Lang, Messages, MessagesApi, MessagesImpl}
-import play.api.test.FakeRequest
-import testhelpers.MockAuthUtil
+import play.api.test.Injecting
+import play.twirl.api.Html
+import testhelpers.{AtedViewSpec, MockAuthUtil}
 
-class peakGuidanceSpec extends FeatureSpec with GivenWhenThen
-  with MockAuthUtil with GuiceOneAppPerTest {
+class peakGuidanceSpec extends AtedViewSpec with MockAuthUtil with Injecting {
 
-  implicit val request = FakeRequest()
   implicit val appConfig: ApplicationConfig = mock[ApplicationConfig]
-  implicit lazy val authContext: StandardAuthRetrievals = organisationStandardRetrievals
-  implicit lazy val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
-  implicit lazy val messages: Messages = MessagesImpl(Lang("en-GB"), messagesApi)
 
-  feature("The user is viewing returns guidance during peak") {
+  val injectedView: peakGuidance = inject[views.html.helpers.peakGuidance]
+  val view: Html = injectedView(true, 2020, 2019)
 
-    lazy val view = views.html.helpers.peakGuidance(
-      duringPeak = true,
-      currentYear = 2020,
-      taxYearStartingYear = 2019
-    )
-    lazy implicit val document: Document = Jsoup.parse(view.body)
+  "Peak guidance view" when {
 
-    scenario("User views the returns guidance during peak") {
+    "The user is viewing returns guidance during peak" must {
 
-      Given("the user views the return guidance")
-      When("the current date is during peak period")
-      Then("the peak period content is displayed")
+      "have the current date during peak period and show correct content" in {
 
-      assert(document.select("strong").text() === "Deadline for 2020 to 2021 returns: 30 April 2020.")
-      assert(document.select("p").first.text() === "This is the deadline for returns and payments for all " +
-        "ATED-eligible properties that you own on 1 April 2020.")
-      assert(document.select("p").last().text() === "Returns for newly acquired ATED properties must be " +
-        "sent to HMRC within 30 days of the date of acquisition (90 days from start date for new builds).")
-
+        assert(doc.select("strong").text() === "Warning Deadline for 2020 to 2021 returns: 30 April 2020.")
+        assert(doc.select("p").first.text() === "This is the deadline for returns and payments for all " +
+          "ATED-eligible properties that you own on 1 April 2020.")
+        assert(doc.select("p").last().text() === "Returns for newly acquired ATED properties must be " +
+          "sent to HMRC within 30 days of the date of acquisition (90 days from start date for new builds).")
+      }
     }
-  }
 
-  feature("The user is viewing returns outside of peak") {
+    "The user is viewing returns outside of peak" must {
 
-    lazy val view = views.html.helpers.peakGuidance(
-      duringPeak = false,
-      currentYear = 2020,
-      taxYearStartingYear = 2020
-    )
+      val view: Html = injectedView(false, 2020, 2020)
 
-    lazy implicit val document: Document = Jsoup.parse(view.body)
+      "have the current date outside peak period and show correct content" in {
 
-    scenario("The user is viewing returns guidance outside of peak") {
-
-      Given("the user views the return guidance")
-      When("the current date is outside of peak period")
-      Then("the outside of peak period guidance is displayed")
-
-      assert(document.select("strong").text() === "Returns for newly acquired ATED properties " +
-        "must be sent to HMRC within 30 days (90 days for new builds).")
-      assert(document.select("p").first.text() === "Returns for 2021 to 2022 are due by 30 April 2021.")
-
+        assert(doc(view).select("strong").text() === "Warning Returns for newly acquired ATED properties " +
+          "must be sent to HMRC within 30 days (90 days for new builds).")
+        assert(doc(view).select("p").first.text() === "Returns for 2021 to 2022 are due by 30 April 2021.")
+      }
     }
   }
 }
