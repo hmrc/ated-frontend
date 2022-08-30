6f9b4d95458125e20ffb8eeb147342719962980d
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-08-30 12:06:56 +0100

    DL-8368 dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "5.25.0",
-    "uk.gov.hmrc"       %% "play-ui"                    % "9.10.0-play-28",
+    "uk.gov.hmrc"       %% "play-ui"                    % "9.11.0-play-28",
     "uk.gov.hmrc"       %% "play-partials"              % "8.3.0-play-28",
     "uk.gov.hmrc"       %% "domain"                     % "8.1.0-play-28",
     "uk.gov.hmrc"       %% "http-caching-client"        % "9.6.0-play-28",
@@ -28,7 +28,7 @@ object AppDependencies {
         "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "5.25.0"            % scope,
         "org.scalatestplus.play"       %% "scalatestplus-play"          % "5.1.0"             % scope,
         "org.pegdown"                  %  "pegdown"                     % "1.6.0"             % scope,
-        "org.jsoup"                    %  "jsoup"                       % "1.15.2"            % scope,
+        "org.jsoup"                    %  "jsoup"                       % "1.15.3"            % scope,
         "com.typesafe.play"            %% "play-test"                   % PlayVersion.current % scope,
         "org.scalatestplus"            %% "mockito-3-12"                % "3.2.10.0"          % scope,
         "org.mockito"                  %  "mockito-core"                % "4.7.0"             % scope,
