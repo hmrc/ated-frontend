3d3bb44e6f317419856e56988894c1b9a72f1f74
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2023-02-24 11:31:11 +0000

    DL-9733 dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,8 +12,8 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-partials"              % "8.3.0-play-28",
     "uk.gov.hmrc"       %% "domain"                     % "8.1.0-play-28",
     "uk.gov.hmrc"       %% "http-caching-client"        % "9.6.0-play-28",
-    "com.typesafe.play" %% "play-json-joda"             % "2.9.3",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "6.3.0-play-28"
+    "com.typesafe.play" %% "play-json-joda"             % "2.9.4",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "6.6.0-play-28"
   )
 
   trait TestDependencies {
@@ -26,13 +26,13 @@ object AppDependencies {
       override lazy val test: Seq[ModuleID] = Seq(
         "uk.gov.hmrc"                  %% "bootstrap-test-play-28"      % "5.25.0"            % scope,
         "org.scalatestplus.play"       %% "scalatestplus-play"          % "5.1.0"             % scope,
-        "org.jsoup"                    %  "jsoup"                       % "1.15.3"            % scope,
+        "org.jsoup"                    %  "jsoup"                       % "1.15.4"            % scope,
         "com.typesafe.play"            %% "play-test"                   % PlayVersion.current % scope,
         "org.scalatestplus"            %% "mockito-3-12"                % "3.2.10.0"          % scope,
         "org.mockito"                  %  "mockito-core"                % "4.11.0"            % scope,
         "com.github.tomakehurst"       %  "wiremock-jre8"               % "2.35.0"            % scope,
         "org.scalamock"                %% "scalamock-scalatest-support" % "3.6.0"             % scope,
-        "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.14.1"            % scope
+        "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.14.2"            % scope
       )
     }.test
   }
