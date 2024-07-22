ba472e9870e50212b34f92e305ba1e4454682421
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2024-07-22 14:47:30 +0100

    DL-14093, dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,9 +10,9 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc" %% "play-partials-play-30"       % "10.0.0",
-    "uk.gov.hmrc" %% "domain-play-30"              % "9.0.0",
-    "uk.gov.hmrc" %% "http-caching-client-play-30" % "12.0.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"  % "10.1.0"
+    "uk.gov.hmrc" %% "domain-play-30"              % "10.0.0",
+    "uk.gov.hmrc" %% "http-caching-client-play-30" % "12.1.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"  % "10.5.0"
   )
 
   val test: Seq[ModuleID] = Seq(
