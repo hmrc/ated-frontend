e800b9fd0ef3eaa1f7c79daeb99aa8c4a0babbb3
nehakannaujia1 <50657531+nehakannaujia1@users.noreply.github.com>
2024-09-10 15:01:28 +0100

    Code clean

--- test/controllers/propertyDetails/PropertyDetailsExitControllerSpec.scala
+++ test/controllers/propertyDetails/PropertyDetailsExitControllerSpec.scala
@@ -16,27 +16,23 @@
 
 package controllers.propertyDetails
 
-import builders.{PropertyDetailsBuilder, SessionBuilder}
+import builders.{SessionBuilder}
 import config.ApplicationConfig
 import connectors.{BackLinkCacheConnector, DataCacheConnector}
 import controllers.auth.AuthAction
-import models.PropertyDetailsNewValuation
 import org.mockito.ArgumentMatchers
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.i18n.{Lang, MessagesApi, MessagesImpl}
-import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.MessagesControllerComponents
-import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import services.{PropertyDetailsCacheSuccessResponse, PropertyDetailsService, ServiceInfoService}
+import services.{PropertyDetailsService}
 import testhelpers.MockAuthUtil
 import uk.gov.hmrc.auth.core.AffinityGroup
 import uk.gov.hmrc.http.HeaderCarrier
 import utils.AtedConstants.DelegatedClientAtedRefNumber
-import views.html.BtaNavigationLinks
 import views.html.propertyDetails.propertyDetailsExit
 
 import java.util.UUID
@@ -51,12 +47,8 @@ class PropertyDetailsExitControllerSpec extends PlaySpec with GuiceOneServerPerS
   val mockPropertyDetailsService: PropertyDetailsService = mock[PropertyDetailsService]
   val mockDataCacheConnector: DataCacheConnector = mock[DataCacheConnector]
   val mockBackLinkCacheConnector: BackLinkCacheConnector = mock[BackLinkCacheConnector]
-  val mockDateOfRevalueController: PropertyDetailsDateOfRevalueController = mock[PropertyDetailsDateOfRevalueController]
-  val mockExitController: PropertyDetailsExitController = mock[PropertyDetailsExitController]
   val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
   lazy implicit val messages: MessagesImpl = MessagesImpl(Lang("en-GB"), messagesApi)
-  val btaNavigationLinksView: BtaNavigationLinks = app.injector.instanceOf[BtaNavigationLinks]
-  val mockServiceInfoService: ServiceInfoService = mock[ServiceInfoService]
   val injectedViewInstance: propertyDetailsExit = app.injector.instanceOf[views.html.propertyDetails.propertyDetailsExit]
 
 
@@ -76,11 +68,8 @@ class PropertyDetailsExitControllerSpec extends PlaySpec with GuiceOneServerPerS
       mockBackLinkCacheConnector,
       injectedViewInstance
     )
-
-    val customBtaNavigationLinks = btaNavigationLinksView()(messages, mockAppConfig)
-
+    
     val userId = s"user-${UUID.randomUUID}"
-    val propertyDetails = PropertyDetailsBuilder.getPropertyDetails("1", Some("z11 1zz")).copy(value = None)
   }
 
   "PropertyDetailsExitController.view" must {
@@ -99,13 +88,10 @@ class PropertyDetailsExitControllerSpec extends PlaySpec with GuiceOneServerPerS
           val authMock = authResultDefault(AffinityGroup.Organisation, defaultEnrolmentSet)
           setAuthMocks(authMock)
           when(mockAppConfig.newRevaluedFeature).thenReturn(true)
-          when(mockServiceInfoService.getPartial(ArgumentMatchers.any(), ArgumentMatchers.any(), ArgumentMatchers.any())).thenReturn(Future.successful(customBtaNavigationLinks))
           when(mockDataCacheConnector.fetchAtedRefData[String](ArgumentMatchers.eq(DelegatedClientAtedRefNumber))(ArgumentMatchers.any(), ArgumentMatchers.any())).thenReturn(Future.successful(Some("XN1200000100001")))
           when(mockDataCacheConnector.fetchAndGetFormData[Boolean](ArgumentMatchers.any())
             (ArgumentMatchers.any(), ArgumentMatchers.any())).thenReturn(Future.successful(None))
           when(mockBackLinkCacheConnector.fetchAndGetBackLink(ArgumentMatchers.any())(ArgumentMatchers.any())).thenReturn(Future.successful(None))
-          when(mockPropertyDetailsService.retrieveDraftPropertyDetails(ArgumentMatchers.any())(ArgumentMatchers.any(), ArgumentMatchers.any()))
-            .thenReturn(Future.successful(PropertyDetailsCacheSuccessResponse(propertyDetails)))
           val result = testController.view().apply(SessionBuilder.buildRequestWithSession(userId))
           status(result) mustBe OK
         }
